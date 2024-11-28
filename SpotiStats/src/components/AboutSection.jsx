84aac5bab4c2f877e5f23193ecdb9edd3b3da6bc
ashzhang1 <ashzhang1@gmail.com>
2024-11-28 19:03:29 +1100

    made intial landing page responsive and started on about section

---
+++ SpotiStats/src/components/AboutSection.jsx
@@ -0,0 +1,56 @@
+import "../styles/components/AboutSection.css";
+import analyticsIcon from "../assets/chart.svg";
+import reactIcon from "../assets/reactIcon.svg";
+import spotifyIcon from "../assets/spotifyIcon.svg";
+import viteIcon from "../assets/viteIcon.svg";
+
+export default function AboutSection() {
+  return (
+    <div className="about-section-container">
+      <div className="about-main-title-container">
+        <h2 className="about-main-title">
+          Deep dive into your musical story with personalised insights and
+          data-driven discovery.
+        </h2>
+      </div>
+
+      <div className="about-features-container">
+        <img
+          src={analyticsIcon}
+          alt="Analytics chart icon representing data visualization features"
+        />
+        <p className="about-description">
+          Utilising various charts, visualise your Spotify listening patterns,
+          encompassing your most recent favourite genres, artists, tracks as
+          well as music recommendations.
+        </p>
+      </div>
+
+      <div className="tech-stack-container">
+        <h2 className="about-project-title">About this project</h2>
+        <p className="about-project-description">
+          The purpose of this project was to develop my front-end web
+          development skills with React and showcase my knowledge of working
+          with APIs. Although relatively a simple website, I am proud of what I
+          have made here and hope you have had a pleasant experience using it.
+        </p>
+
+        <div className="tech-stack-section">
+          <h3 className="tech-stack-title">Tech-Stack:</h3>
+          <div className="icon-label-container">
+            <img src={reactIcon} alt="React icon" />
+            <span>React</span>
+          </div>
+          <div className="icon-label-container">
+            <img src={viteIcon} alt="React icon" />
+            <span>Vite</span>
+          </div>
+          <div className="icon-label-container">
+            <img src={spotifyIcon} alt="React icon" />
+            <span>Spotify API</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- SpotiStats/src/pages/LandingPage.jsx
+++ SpotiStats/src/pages/LandingPage.jsx
@@ -1,17 +1,24 @@
 import "../styles/pages/LandingPage.css";
 import logo from "../assets/logo.svg";
+import AboutSection from "../components/AboutSection";
 
 export default function LandingPage() {
   return (
     <div className="landing-page-container">
-      <div className="landing-page-content">
-        <div className="landing-page-logo-container">
-          <img src={logo} />
-          <h1 className="logo-title">Spotistics</h1>
+      <section className="hero-section">
+        <div className="landing-page-content">
+          <div className="landing-page-logo-container">
+            <img src={logo} className="logo-image" alt="SpotiStats Logo" />
+            <h1 className="logo-title">SpotiStats</h1>
+          </div>
+          <h2 className="landing-page-subtitle">Chart your musical voyage</h2>
         </div>
-        <h2 className="landing-page-subtitle">Chart your musical voyage</h2>
-      </div>
-      <button className="get-started-button">Get Started</button>
+        <button className="get-started-button">Get Started</button>
+      </section>
+
+      <section className="about-section">
+        <AboutSection />
+      </section>
     </div>
   );
 }
